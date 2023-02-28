3ffbc6803892e02ec40a3f5f4d3303fe223c6ccf
ThankedDeer <117323510+ThankedDeer@users.noreply.github.com>
2023-02-28 09:57:44 -0600

    lalo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { from } from 'rxjs';
 
 import { AppComponent } from './app.component';
 import { PalindromoComponent } from './palindromo/palindromo.component';
 import { PotenciaComponent } from './potencia/potencia.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,13 @@ import { PotenciaComponent } from './potencia/potencia.component';
     PotenciaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   
   ],
   providers: [],
